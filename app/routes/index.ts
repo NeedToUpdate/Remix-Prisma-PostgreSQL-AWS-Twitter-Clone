ecd0b3c1963d258732c4227c516394170af2a1fd
Artem <updatethatpl0x@gmail.com>
2022-12-21 13:06:38 -0500

    index.tsx -> index.ts

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,7 +1,8 @@
 import type { LoaderFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
 import { requireUserId } from "~/utils/auth.server";
 
 export const loader: LoaderFunction = async ({ request }) => {
   await requireUserId(request);
-  return null;
+  return redirect("/home");
 };
